46098bc898959ace0baa32b0a788a738583106da
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-06-02 01:37:11 +0300

    Add public device IDs to syncthing

--- syncthing.nix
+++ syncthing.nix
@@ -8,12 +8,14 @@
 
 		settings = {
 			devices = {
+				"helium" = { id = "2MRUBSY-NHXYMAW-SY22RHP-CNNMHKR-DPDKMM4-2XV5F6M-6KSNLQI-DD4EOAM"; };
+				"nixos-cpu" = { id = "ZX35ARB-3ULEUV3-NNUEREF-DEDWOJU-GE7A4PP-T7O43NI-SU564OD-E26HHA4"; };
 			};
 
 			folders =
 			let
 				default = {
-					devices = [];
+					devices = [ "helium" "nixos-cpu" ];
 					versioning = {
 						type = "trashcan";
 						params.cleanoutDays = "30";
