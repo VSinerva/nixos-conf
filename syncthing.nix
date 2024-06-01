538642617b3386e3f9fd480f99e295b4ad4e983b
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-06-02 01:43:14 +0300

    Add adresses to devices

--- syncthing.nix
+++ syncthing.nix
@@ -8,8 +8,14 @@
 
 		settings = {
 			devices = {
-				"helium" = { id = "2MRUBSY-NHXYMAW-SY22RHP-CNNMHKR-DPDKMM4-2XV5F6M-6KSNLQI-DD4EOAM"; };
-				"nixos-cpu" = { id = "ZX35ARB-3ULEUV3-NNUEREF-DEDWOJU-GE7A4PP-T7O43NI-SU564OD-E26HHA4"; };
+				"helium" = {
+					id = "2MRUBSY-NHXYMAW-SY22RHP-CNNMHKR-DPDKMM4-2XV5F6M-6KSNLQI-DD4EOAM";
+					addresses = [ "tcp://helium.vsinerva.fi:22000" ];
+				};
+				"nixos-cpu" = {
+					id = "ZX35ARB-3ULEUV3-NNUEREF-DEDWOJU-GE7A4PP-T7O43NI-SU564OD-E26HHA4";
+					addresses = [ "tcp://nixos-cpu.vsinerva.fi:22000" ];
+				};
 			};
 
 			folders =
