597c4aa2e2186c50cc73977e20b544751fee88ad
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-07-11 16:43:26 +0300

    Add initial lithium config

---
+++ machine-confs/lithium.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  networking = {
+    hostName = "lithium";
+
+imports = [
+    ../base.nix
+    ../users/vili.nix
+    ../desktop.nix
+    ../development.nix
+    ../services/redshift.nix
+    ../hardware-specific/usb-automount.nix
+  ];
+}
