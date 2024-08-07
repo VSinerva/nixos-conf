78f0a061e05a21d58067cddd3aef4e37e9a183b0
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-08-07 15:51:07 +0300

    Prevent user from being locked out with generic machine-conf

--- machine-confs/generic.nix
+++ machine-confs/generic.nix
@@ -10,6 +10,9 @@
   #Many installs will need this, and it won't hurt either way
   services.qemuGuest.enable = true;
 
+  #Prevent user from being locked out of the system before switching to proper config
+  users.mutableUsers = pkgs.lib.mkForce true;
+
   swapDevices = [
     {
       device = "/var/lib/swapfile";
