6812790eb7361012725da9813373b8cd0cd1f840
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-08-28 13:18:09 +0300

    Improve custom ISO experience

--- machine-confs/generic.nix
+++ machine-confs/generic.nix
@@ -9,4 +9,7 @@
 
   #Many installs will need this, and it won't hurt either way
   services.qemuGuest.enable = true;
+
+  #Prevent user from being locked out of the system before switching to proper config
+  users.mutableUsers = pkgs.lib.mkForce true;
 }
