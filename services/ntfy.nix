f8655e5440d320c023efd1a0c5f03b44a681f982
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-09-06 20:44:38 +0300

    Add basic ntfy conf

--- services/ntfy.nix
+++ services/ntfy.nix
@@ -1,1 +1,19 @@
-{ config, pkgs, ... }: { }
+{ config, pkgs, ... }:
+{
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+  networking.firewall.allowedUDPPorts = [ 443 ];
+
+  services.ntfy = {
+    enable = true;
+    settings = {
+      base-url = "https://ntfy.vsinerva.fi";
+      listen-https = ":443";
+      key-file = "/var/lib/ntfy/privkey.pem";
+      cert-file = "/var/lib/ntfy/fullchain.pem";
+      cache-file = "/var/lib/ntfy/notif-cache.sqlite";
+    };
+  };
+}
