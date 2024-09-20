d13e4181b62778d77d0d7c47e2a5acf5753fc319
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-09-20 21:15:58 +0300

    Add minimal NAT64 conf

---
+++ machine-confs/nat64.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+{
+  networking.hostName = "nat64";
+
+  imports = [
+    ../base.nix
+    ../services/nat64.nix
+  ];
+
+  # HARDWARE SPECIFIC
+  services.qemuGuest.enable = true;
+}
