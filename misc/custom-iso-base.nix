9df3c02e55353c69cc6cec8c4a996b21b8e3220a
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-10-15 20:11:43 +0300

    Fix custom ISO onlykey

--- misc/custom-iso-base.nix
+++ misc/custom-iso-base.nix
@@ -59,8 +59,10 @@ in
     (with pkgs; [
       (onlykey.override (prev: {
         node_webkit = prev.node_webkit.overrideAttrs {
-          src = fetchurl { url = "https://dl.nwjs.io/v0.71.1/nwjs-v0.71.1-linux-x64.tar.gz"; };
-          hash = "sha256-bnObpwfJ6SNJdOvzWTnh515JMcadH1+fxx5W9e4gl/4=";
+          src = fetchurl {
+            url = "https://dl.nwjs.io/v0.71.1/nwjs-v0.71.1-linux-x64.tar.gz";
+            hash = "sha256-bnObpwfJ6SNJdOvzWTnh515JMcadH1+fxx5W9e4gl/4=";
+          };
         };
       }))
 
