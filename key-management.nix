859edb1db550f3f89045d3d8f2226b142267e914
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-10-15 19:49:04 +0300

    Add key management tools to helium

--- key-management.nix
+++ key-management.nix
@@ -8,7 +8,15 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    onlykey
+    (onlykey.override (prev: {
+      node_webkit = prev.node_webkit.overrideAttrs {
+        src = fetchurl {
+          url = "https://dl.nwjs.io/v0.71.1/nwjs-v0.71.1-linux-x64.tar.gz";
+          hash = "sha256-bnObpwfJ6SNJdOvzWTnh515JMcadH1+fxx5W9e4gl/4=";
+        };
+      };
+    }))
+
     onlykey-cli
     onlykey-agent
   ];
@@ -20,11 +28,10 @@
     pinentryPackage = pkgs.pinentry-curses;
   };
 
-  hardware.onlykey.enable = true;
-  #  services.udev.extraRules = with pkgs; ''
-  #    ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{ID_MM_DEVICE_IGNORE}="1"
-  #    ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{MTP_NO_PROBE}="1"
-  #    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", OWNER:="vili", RUN+="${onlykey-cli}/bin/onlykey-cli settime"
-  #    KERNEL=="ttyACM*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", OWNER:="vili", RUN+="${onlykey-cli}/bin/onlykey-cli settime"
-  #  '';
+  services.udev.extraRules = with pkgs; ''
+    ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{ID_MM_DEVICE_IGNORE}="1"
+    ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{MTP_NO_PROBE}="1"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", OWNER:="vili", RUN+="${onlykey-cli}/bin/onlykey-cli settime"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", OWNER:="vili", RUN+="${onlykey-cli}/bin/onlykey-cli settime"
+  '';
 }
--- misc/custom-iso-base.nix
+++ misc/custom-iso-base.nix
@@ -57,8 +57,14 @@ in
 
   environment.systemPackages =
     (with pkgs; [
+      (onlykey.override (prev: {
+        node_webkit = prev.node_webkit.overrideAttrs {
+          src = fetchurl { url = "https://dl.nwjs.io/v0.71.1/nwjs-v0.71.1-linux-x64.tar.gz"; };
+          hash = "sha256-bnObpwfJ6SNJdOvzWTnh515JMcadH1+fxx5W9e4gl/4=";
+        };
+      }))
+
       cryptsetup
-      onlykey
       onlykey-cli
       onlykey-agent
     ])
