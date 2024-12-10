c38e36bb7e1f183d30db7e522bedf2c7689e44de
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-12-10 15:07:27 +0200

    Update dhcp cluster node conf

--- machine-confs/dhcp1.nix
+++ machine-confs/dhcp1.nix
@@ -10,18 +10,18 @@
     ];
   };
 
-  users.users.node = {
+  users.users.ds_dhcp = {
     isNormalUser = true;
-    home = "/home/node";
+    home = "/home/ds_dhcp";
     description = "DHCP Cluster Node";
-    uid = 1001;
+    uid = 1002;
     openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys ++ [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7NmrrP7rSU76qr/JEzdJYaulFYmn6f/y0YPevTjK2B simo@capscience.fi"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjdik9FU/tzpmAGJde/dh/RAuj+RatYxcwJGtIYVlbk cardno:19_036_796"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4BHc8GiptScZ6vmdq1incgrEKFSjTvybktvc1FGmMG roysko.juho@gmail.com"
     ];
   };
-  users.groups.worker.gid = 1001;
+  users.groups.ds_dhcp.gid = 1002;
 
   environment.systemPackages = with pkgs; [
     gcc
@@ -38,7 +38,7 @@
         color = "green";
         command = ''
           ${pkgs.figlet}/bin/figlet "DHCP Node";
-          ${pkgs.coreutils-full}/bin/echo -e "$(${pkgs.procps}/bin/ps --User node --user node --forest --format start_time=STARTED,time=CPU_TIME,%cpu,%mem,comm)";
+          ${pkgs.coreutils-full}/bin/echo -e "$(${pkgs.procps}/bin/ps --User ds_dhcp --user ds_dhcp --forest --format start_time=STARTED,time=CPU_TIME,%cpu,%mem,comm)";
         '';
       };
       uptime.prefix = "System has been running for";
