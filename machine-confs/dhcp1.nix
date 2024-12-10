a51bdd7c131f281dc5b907cae4ec36697f2cbe25
Vili Sinervä <vili.m.sinerva@gmail.com>
2024-12-10 14:48:17 +0200

    Update dhcp cluster node conf

--- machine-confs/dhcp1.nix
+++ machine-confs/dhcp1.nix
@@ -18,11 +18,13 @@
     openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys ++ [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7NmrrP7rSU76qr/JEzdJYaulFYmn6f/y0YPevTjK2B simo@capscience.fi"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjdik9FU/tzpmAGJde/dh/RAuj+RatYxcwJGtIYVlbk cardno:19_036_796"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4BHc8GiptScZ6vmdq1incgrEKFSjTvybktvc1FGmMG roysko.juho@gmail.com"
     ];
   };
   users.groups.worker.gid = 1001;
 
   environment.systemPackages = with pkgs; [
+    gcc
     rustc
     cargo
   ];
