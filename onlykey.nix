b9ac082083dcd1d414f85ef99b6d00dc3f2e59ee
Vili Sinervä <vili.m.sinerva@gmail.com>
2025-01-13 18:04:50 +0200

    Explicitly change nwjs version for onlykey

--- onlykey.nix
+++ onlykey.nix
@@ -10,6 +10,7 @@
   environment.systemPackages = with pkgs; [
     (onlykey.override (prev: {
       node_webkit = prev.node_webkit.overrideAttrs {
+        version = "0.71.1";
         src = fetchurl {
           url = "https://dl.nwjs.io/v0.71.1/nwjs-v0.71.1-linux-x64.tar.gz";
           hash = "sha256-bnObpwfJ6SNJdOvzWTnh515JMcadH1+fxx5W9e4gl/4=";
